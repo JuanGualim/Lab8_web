e01aecdc3a03b372be2b1361c9769a8fc95a7fe4
JuanGualim <gua24852@uvg.edu.gt>
2026-05-18 17:44:56 -0600

    Implementaci+on del componente PasswordStrengthMeter

--- password-strengh-meter/src/components/PasswordStrength.jsx
+++ password-strengh-meter/src/components/PasswordStrength.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'
+import { getPasswordStrength } from '../utils/passwordStrength'
+
 export default function PasswordStrengthMeter() {
+  const [password, setPassword] = useState('')
+
+  const strength = getPasswordStrength(password)
+
   return (
     <div>
+      <label htmlFor="password">Password</label>
 
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <p>{strength}</p>
     </div>
   )
 }
